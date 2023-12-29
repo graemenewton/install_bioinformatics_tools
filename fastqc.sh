583476d6735986ccc9e9c82acd44586318eea57d
Graeme Newton <73535025+graemenewton@users.noreply.github.com>
2023-12-29 11:58:44 +0000

    Update fastqc.sh

--- fastqc.sh
+++ fastqc.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env sh
 
+# Download FastQC v0.12.1
 wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.12.1.zip
 
+# Unzip FastQC
 unzip fastqc_v0.12.1.zip
 
+# Delete zipped folder
 rm fastqc_v0.12.1.zip
 
+# Move into folder
 cd fastqc_v0.12.1
 
+# Make fastqc executable
 chmod 755 fastqc
 
+# Add shortcut to main directory (update paths)
 sudo ln -s /path/to/FastQC/fastqc /usr/local/bin/fastqc
