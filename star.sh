66716652056058b5a1a9e48830014f0adcd0a258
Graeme Newton <73535025+graemenewton@users.noreply.github.com>
2023-12-29 13:08:23 +0000

    Update star.sh

--- star.sh
+++ star.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env sh
 
+# Download STAR 2.7.11a
+wget https://github.com/alexdobin/STAR/archive/2.7.11a.tar.gz
+tar -xzf 2.7.11a.tar.gz
+
+# Run STAR
+./STAR-2.7.11a/bin/Linux_x86_64_static/STAR
+
+# Create shortcut in current directory (admin rights required)
+ sudo ln -s ./STAR-2.7.11a/bin/Linux_x86_64_static/STAR ./STAR
+
+# Run
+./STAR
