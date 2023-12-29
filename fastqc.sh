4b63741d32977369254c31a974912bb5e5de3e14
Graeme Newton <73535025+graemenewton@users.noreply.github.com>
2023-12-29 11:39:55 +0000

    Update fastqc.sh

--- fastqc.sh
+++ fastqc.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env sh
-ls
+
+wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.12.1.zip
+
+unzip fastqc_v0.12.1.zip
+
+cd fastqc_v0.12.1
+
+chmod 755 fastqc
+
+sudo ln -s /path/to/FastQC/fastqc /usr/local/bin/fastqc
