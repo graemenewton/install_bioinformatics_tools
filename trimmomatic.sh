c0c35dc35123a65d4ec9328b3544dccf4692dc52
Graeme Newton <73535025+graemenewton@users.noreply.github.com>
2023-12-29 11:51:05 +0000

    Update trimmomatic.sh

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env sh
 
+wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip
 
-http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip
+unzip Trimmomatic-0.39.zip
+
+rm Trimmomatic-0.39.zip
+
+java -jar Trimmomatic-0.39/trimmomatic-0.39.jar
